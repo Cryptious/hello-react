50d665e42b94d3ba3ac85c2c116ac1da956137a0
Martino Wagey <martinowagey@gmail.com>
2018-11-21 11:24:19 +0700

    Component and function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import Header from "./Header";
+import Footer from "./Footer";
 
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <h1>Rendering Elements</h1>
-        <p></p>
+        <Footer />
       </div>
     );
   }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <h4>Ini Halaman Footer </h4>
+      <p>@2018</p>
+    </div>
+  );
+}
+
+export default Footer;
