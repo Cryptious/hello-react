36c6c107558eae111d23d2354c61e63d3a2300c9
Cryptious <martinowagey@gmail.com>
2019-01-25 15:29:36 +0700

    Props pada Class Component

--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import Image from "./Image";
 
-
-
 class List extends Component {
   render(){
     return(
       <ol>
-        <Image />
+        <Image linkgambar="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/food1.jpg"
+          lebar="500"
+        />
+        <li>Makanan</li>
+        <Image linkgambar="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/nasipadang.jpg"
+          lebar="400"
+        />
         <li>Nasi Padang</li>
+        <Image  linkgambar="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sate.png"
+          lebar="300"
+        />
         <li>Sate</li>
+        <Image  linkgambar="https://s3-ap-southeast-1.amazonaws.com/niomic/img/sample/sotolamongan.png"
+          lebar="200"
+        />
         <li>Soto Lamongan</li>
-        <li>Sushi</li>
       </ol>
     );
   }
