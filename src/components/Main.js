a7733d30eb05e19108638301b419e462d667d4b1
Cryptious <martinowagey@gmail.com>
2019-04-25 16:45:11 +0700

    add event onChange Text

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,20 +5,46 @@ class Main extends Component{
   constructor(props){
     super(props);
       this.state = {
-        title : "Menu Makanan"
+        title : "Menu Makanan",
+        title2: "Menu Minuman",
+        inputValue: "Nasi Padang Uhuy"
       };
       this.rubahData = this.rubahData.bind(this);
+      this.handleChange = this.handleChange.bind(this);
   }
 
+  // Cara menggunakan State
+  //1
+  // rubahData(){
+  //   this.setState({title:"Pilih Makanan"})
+  // }
+
+  //2
   rubahData(){
-    this.setState({title:"Pilih Makanan"})
+    this.setState((state, props) => {
+      return {
+        title: state.title2,
+        title2: state.title
+      };
+    });
+  }; //disarankan menggunakan state arrow function
+  // End Cara penulisan State
+
+  handleChange(e){
+    console.log(e.target.value);
   }
 
   render(){
     return(
       <div>
         <h3>{this.state.title}</h3>
+        <h2>{this.state.title2}</h2>
         <button onClick={this.rubahData}>Rubah Data</button>
+
+        <br />
+        <br />
+
+        <input type="text" value={this.state.inputValue} onChange={this.handleChange} />
       </div>
     );
   }
